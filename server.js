6cf61c37cb5e1f49985872cfb5634f830846bb54
gbibeaul <g.bibeaulaviolette@gmail.com>
2017-06-01 14:06:21 -0400

    linked front and back

---
+++ server.js
@@ -0,0 +1,34 @@
+const { createStore } = require('redux')
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY || 'ZWW2k1muXdiCuwhzqN9RvORmV',
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET || 'K78xOcKfImZuNOQIPCt8qc7Iz096apPxbBfnTfQ2D1cRSSlMZe',
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY || '378169226-ZqXLg4z0ouFvosEdmnqk28wlBowiwb78Y0uZ6upu',
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET  || 'Gbi9Fev9hyr6088kUq35kKxPQjisuDADLITDettsEFXV7'
+})
+//
+var tweets = []
+var stream = client.stream('statuses/filter', {track: 'javascript'});
+stream.on('data', (event) => {
+  tweets.length > 5 ? tweets.shift() : null
+  tweets.push(event.text)
+    io.emit('tweets', tweets[tweets.length -1])
+  console.log('new tweet');
+});
+
+stream.on('error', function(error) {
+  throw error;
+});
+
+
+io.on('connection', (socket) => {
+  socket.on('Connected', (msg) => { console.log('this was sent from the fron' + msg)})
+});
+
+http.listen(process.env.PORT || 3000, function(){
+  console.log('server has started');
+});
---
+++ src/Stream.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+const io = require('socket.io-client')
+const socket = io('http://localhost:3001/')
+
+class Stream extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { tweets: []}
+  }
+
+  componentDidMount = () => {
+    socket.emit('Connected', 'Connection from front')
+    socket.on('tweets', newTweet => {
+      let tweets = this.state.tweets.reverse()
+      tweets.length >= 5 ? tweets.shift() : tweets
+      tweets.push(newTweet);
+      this.setState({ tweets : tweets})
+      });
+  }
+
+  render (){
+    return (
+      <article id="first" className="container box style1 right">
+        <div class="inner">
+          <ul>
+          {this.state.tweets.map((item,i) => <li className="tweet" key={i}>{item}</li>)}
+          </ul>
+          <header>
+            <h2>Javascript</h2>
+          </header>
+        </div>
+      </article>
+    );
+  }
+}
+
+export default Stream;
