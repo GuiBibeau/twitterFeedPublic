01ca4dba7de16038a1d029df5299b28d4018a804
gbibeaul <g.bibeaulaviolette@gmail.com>
2017-06-02 00:39:12 -0400

    changed socket address

--- src/Stream.js
+++ src/Stream.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 const io = require('socket.io-client')
-const socket = io('http://localhost:3001/')
+const socket = io('https://tweetstreamserver.herokuapp.com/')
 
 class Stream extends Component {
   constructor(props) {
